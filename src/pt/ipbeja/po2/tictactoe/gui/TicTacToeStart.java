2692ae280e812133599b90cdc107880465adcb4c
PedroTauzen <pedrotauzen@gmail.com>
2023-06-24 00:06:59 +0100

    Initial commit

---
+++ src/pt/ipbeja/po2/tictactoe/gui/TicTacToeStart.java
@@ -0,0 +1,16 @@
+package pt.ipbeja.po2.tictactoe.gui;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TicTacToeStart extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
